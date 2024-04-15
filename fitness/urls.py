07f19a2e5fdc7413996b17b82a72f66d33a80852
SamirAlizada <samir.elizade5555@gmail.com>
2024-04-15 19:40:07 +0400

    urls update

--- fitness/urls.py
+++ fitness/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', add_student, name='add_student'),
+    path('add-student', add_student, name='add_student'),
     path('add-trainer', add_trainer, name='add_trainer'),
     path('add-monthprice', add_monthlypricing, name='add_monthlypricing'),
 
     path('trainer-list/', trainer_list, name='trainer_list'),
-    path('student-list/', student_list, name='student_list'),
+    path('', student_list, name='student_list'),
     path('bar-list/', bar_list, name='bar_list'),
 
 ]
